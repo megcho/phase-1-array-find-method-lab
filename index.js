b3559767946ba17c686b558e6a24a76965df58d2
megan cho <meganlisacho@gmail.com>
2021-04-18 21:33:17 -0500

    commit tree 206d022675657c126e2c70387a77a1be7208e280

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 
+// // Write a function called superbowlWin() in index.js:
+
+// // The function should receive 1 argument, an Array of JavaScript Objects
+// // Each object has two properties: year and result
+// // It should use find() to test each Object to see if the result is "W" — a win!
+// // It should return the year when the win occurred (if it occurred at all!)
+// // If no win is found, it should return, sadly, undefined
+
+// const record = [
+//     {year: "2018", result: "L"},
+//     {year: "2017", result: "W"},
+//     {year: "2016", result: "N/A"}
+//     //...
+//   ] 
+
+function superbowlWin(array) {
+    let win = array.find(array => array.result === "W");
+    if (win){
+        return win.year
+    } else {
+        return undefined;
+    }
+}
